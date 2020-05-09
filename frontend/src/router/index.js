8999d828d47922d88c180afde417f7fe469852fa
Jay Meck <jmeck@p-r-i.org>
2020-05-09 15:39:07 -0400

    Updated my website into a backend and a frontend.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Computers from '../views/Computers.vue'
 
 Vue.use(VueRouter)
 
@@ -16,9 +17,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/Computers.vue')
-    }
+    component: Computers
   },
   {
     path: '/cellphones',
